73edd3d374793dd2854de8ec9ca67121564ffe0e
Kristina Jones <kristinalj330@gmail.com>
2017-03-20 14:16:57 +0000

    Tiny Turtle

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ forward(50);
 
 // Type your function call below
 
-
+function triangle() {
+    
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+    forward(75);
+    }
+    function rectangle(){
+        forward(75);
+        left(270);
+        forward(120);
+        left(270);
+        forward(75);
+        left(270);
+        forward(120);
+        
+    }
+rectangle();
 stamp();
